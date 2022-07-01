997f9cac812e3c2d7b90563f8c134e31a2ace1ee
Dmitry Krayniy <krdima92@gmail.com>
2022-07-01 10:31:36 +0400

    Initial commit

---
+++ checkout.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/env pwsh
+
+git clone git@github.com:pip-services-users2/facade-users-node.git
+git clone git@github.com:pip-services-users2/service-smssettings-node.git
+git clone git@github.com:pip-services-users2/service-roles-node.git
+git clone git@github.com:pip-services-users2/service-passwords-node.git
+git clone git@github.com:pip-services-users2/service-orgroles-node.git
+git clone git@github.com:pip-services-users2/service-organizations-node.git
+git clone git@github.com:pip-services-users2/service-msgdistribution-node.git
+git clone git@github.com:pip-services-users2/service-emailsettings-node.git
+git clone git@github.com:pip-services-users2/service-activities-node.git
+git clone git@github.com:pip-services-users2/client-smssettings-node.git
+git clone git@github.com:pip-services-users2/client-roles-node.git
+git clone git@github.com:pip-services-users2/client-passwords-node.git
+git clone git@github.com:pip-services-users2/client-organizations-node.git
+git clone git@github.com:pip-services-users2/client-msgdistribution-node.git
+git clone git@github.com:pip-services-users2/client-invitations-node.git
+git clone git@github.com:pip-services-users2/client-emailsettings-node.git
+git clone git@github.com:pip-services-users2/client-activities-node.git
+git clone git@github.com:pip-services-users2/client-sessions-node.git
+git clone git@github.com:pip-services-users2/client-orgroles-node.git
+git clone git@github.com:pip-services-users2/service-invitations-node.git
+git clone git@github.com:pip-services-users2/client-accounts-node.git
+git clone git@github.com:pip-services-users2/service-accounts-node.git
